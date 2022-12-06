70b0723f2690d6e548c5230e438f33e3b7351f4d
shoeywoopu <2636934648@qq.com>
2022-12-06 11:31:21 +0800

    20301150-zxy-FirstCommit

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,8 +1,42 @@
 #![no_std]
 #![no_main]
-use core::panic::PanicInfo;
+#![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
+use core::arch::global_asm;
+extern crate alloc;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo)-> !{
-    loop{}
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
+mod syscall;
+mod trap;
+mod loader;
+mod config;
+mod task;
+mod timer;
+mod mm;
+
+global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("[Kernel] Hello, world!");
+    trap::init();
+    mm::init();
+    loader::load_apps();
+    trap::enable_timer_interrupt();
+    timer::set_next_trigger();
+    task::run_first_task();
+    panic!("Unreachable in rust_main!");
 }
