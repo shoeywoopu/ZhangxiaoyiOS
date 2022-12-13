2a8395f5602a2aad96d39b798857a6a6e5275fb2
shoeywoopu <2636934648@qq.com>
2022-12-13 13:17:59 +0800

    20301150-zxy-9thCommit-12/13 13:17

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -140,8 +140,21 @@ fn mark_current_exited() {
 }
 
 pub fn suspend_current_and_run_next() {
-    mark_current_suspended();
-    run_next_task();
+    // There must be an application running.
+    let task = take_current_task().unwrap();
+
+    // ---- access current TCB exclusively
+    let mut task_inner = task.inner_exclusive_access();
+    let task_cx_ptr = &mut task_inner.task_cx as *mut TaskContext;
+    // Change status to Ready
+    task_inner.task_status = TaskStatus::Ready;
+    drop(task_inner);
+    // ---- release current PCB
+
+    // push back to ready queue.
+    add_task(task);
+    // jump to scheduling cycle
+    schedule(task_cx_ptr);
 }
 
 pub fn exit_current_and_run_next() {
